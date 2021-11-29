3d2b7157fb081bffba21d740ab61fce919625081
bipinlongjam <41874001+bipinlongjam@users.noreply.github.com>
2021-11-29 18:30:39 +0530

    Update circle.js

--- src/components/circle.js
+++ src/components/circle.js
@@ -1,6 +1,15 @@
-// import using require
+const Shape =require("./shape")
 
 // declare class
+class Circle extends Shape{
+  super(radius){
+    this.radius= radius;
+  }
 
-
+     calculateArea(){
+    console.log('Area');
+     }
+  }
+    
 // export class using module.exports
+  module.exports = Circle;
